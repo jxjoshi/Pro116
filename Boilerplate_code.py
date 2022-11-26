2762853180b92166fc24df9beabcf10355e0c3bc
Jai Joshi <83172501+jxjoshi@users.noreply.github.com>
2022-11-26 10:20:24 -0600

    Add files via upload

---
+++ Boilerplate_code.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def first_flask():
+    return "This is my first flask program"
+@app.route('/flask_2')
+def second_flask():
+    return 'Python is fun!'
+app.run(debug=True)
